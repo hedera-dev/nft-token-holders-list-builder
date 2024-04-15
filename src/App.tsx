f43e8e78bc324a6b49bc7895715dbe27f03224bc
michalrozekariane <156693677+michalrozekariane@users.noreply.github.com>
2024-04-15 10:15:14 +0200

    26-thl-add-placeholder-instead-of-value (#28)
    * feat: placeholder and default value for minAmount changed
    
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>
    
    * fix: placeholder added to dictionary
    
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>
    
    ---------
    
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- src/App.tsx
+++ src/App.tsx
@@ -145,6 +145,7 @@ const App = () => {
     <div className="container mx-auto">
       <h1 className="mt-20 scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">{dictionary.title}</h1>
       <p className="text-center leading-7 [&:not(:first-child)]:mt-6">{dictionary.description}</p>
+
       <div className="mt-5 flex items-center justify-center space-x-2">
         <Label className={`${isAllConditionsRequired && 'text-muted-foreground'}`} htmlFor="isAllConditionsRequired">
           {dictionary.isAllConditionsRequiredLabelLeft}
@@ -154,6 +155,7 @@ const App = () => {
           {dictionary.isAllConditionsRequiredLabelRight}
         </Label>
       </div>
+
       <div className="mb-20 mt-5">
         <HoldersForm
           setFormData={setFormData}
@@ -165,6 +167,7 @@ const App = () => {
           progress={progress}
         />
       </div>
+
       {isFetched || isFetching ? (
         isFetching ? (
           <div className="flex w-full flex-col space-y-3">
